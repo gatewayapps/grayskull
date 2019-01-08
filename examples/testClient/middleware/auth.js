b30b52fe9c0a48f20f1071c14cd117db049c684d
Daniel Gary <daniel@gatewayapps.com>
2019-01-08 16:23:39 -0600

    formatting

--- examples/testClient/middleware/auth.js
+++ examples/testClient/middleware/auth.js
@@ -5,7 +5,7 @@ const fetch = require('isomorphic-fetch')
 const ACCESS_TOKEN_COOKIE_NAME = 'access_token'
 const REFRESH_TOKEN_COOKIE_NAME = 'refresh_token'
 
-exports.requireAuth = async function (req, res, next) {
+exports.requireAuth = async function(req, res, next) {
   if (req.cookies[ACCESS_TOKEN_COOKIE_NAME]) {
     try {
       const decoded = jwt.verify(req.cookies[ACCESS_TOKEN_COOKIE_NAME], config.secret)
@@ -17,7 +17,7 @@ exports.requireAuth = async function (req, res, next) {
       console.error(err)
     }
   }
-  
+
   if (req.cookies[REFRESH_TOKEN_COOKIE_NAME]) {
     // Attempt to verify refreshToken
     const reqOpts = {
@@ -56,7 +56,7 @@ exports.requireAuth = async function (req, res, next) {
   return
 }
 
-exports.completeSignin = async function (req, res) {
+exports.completeSignin = async function(req, res) {
   const reqOpts = {
     method: 'POST',
     headers: {
@@ -107,12 +107,7 @@ function redirectToLogin(res, returnPath) {
     returnPath
   }
   const state = Buffer.from(JSON.stringify(stateObj), 'utf8').toString('base64')
-  const query = [
-    `client_id=${config.clientId}`,
-    'response_type=code',
-    `redirect_uri=${encodeURIComponent(`${config.clientBaseUrl}/signin`)}`,
-    `state=${state}`
-  ]
+  const query = [`client_id=${config.clientId}`, 'response_type=code', `redirect_uri=${encodeURIComponent(`${config.clientBaseUrl}/signin`)}`, `state=${state}`]
   res.redirect(`${config.authServerBaseUrl}/auth?${query.join('&')}`)
   return
 }
