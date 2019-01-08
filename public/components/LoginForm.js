d4882b4aedebe5be549bde3b51af17abd1884a4c
Daniel Gary <daniel@gatewayapps.com>
2019-01-08 16:24:18 -0600

    formatting

--- public/components/LoginForm.js
+++ public/components/LoginForm.js
@@ -5,26 +5,28 @@ import { Mutation } from 'react-apollo'
 import generateFingerprint from '../utils/generateFingerprint'
 
 const LOGIN_MUTATION = gql`
-  mutation LOGIN_MUTATION (
-    $emailAddress: String!,
-    $password: String!,
-    $otpToken: String,
-    $sessionId: String!,
+  mutation LOGIN_MUTATION(
+    $emailAddress: String!
+    $password: String!
+    $otpToken: String
+    $sessionId: String!
     $client_id: Int!
     $response_type: String!
     $redirect_uri: String!
     $state: String
   ) {
-    login(data: {
-      emailAddress: $emailAddress,
-      password: $password,
-      otpToken: $otpToken,
-      sessionId: $sessionId,
-      client_id: $client_id,
-      response_type: $response_type,
-      redirect_uri: $redirect_uri,
-      state: $state
-    }) {
+    login(
+      data: {
+        emailAddress: $emailAddress
+        password: $password
+        otpToken: $otpToken
+        sessionId: $sessionId
+        client_id: $client_id
+        response_type: $response_type
+        redirect_uri: $redirect_uri
+        state: $state
+      }
+    ) {
       success
       message
       otpRequired
@@ -47,7 +49,7 @@ class LoginForm extends PureComponent {
     sessionId: '',
     otpRequired: false,
     message: undefined,
-    backupCodeSent: false,
+    backupCodeSent: false
   }
 
   async componentDidMount() {
@@ -95,72 +97,39 @@ class LoginForm extends PureComponent {
       <Mutation mutation={LOGIN_MUTATION}>
         {(login, { error, loading }) => (
           <div className="card">
-            <div className="card-header">
-              Login to {this.props.client.name}
-            </div>
+            <div className="card-header">Login to {this.props.client.name}</div>
             <div className="card-body">
               <div className="row">
-                <div className="col-3">
-                  {this.props.client.logoImageUrl && (
-                    <img src={this.props.client.logoImageUrl} style={{ width: '100%' }} />
-                  )}
-                </div>
+                <div className="col-3">{this.props.client.logoImageUrl && <img src={this.props.client.logoImageUrl} style={{ width: '100%' }} />}</div>
                 <div className="col-9">
                   {!this.state.otpRequired && (
                     <>
                       <div className="form-group">
                         <label htmlFor="name">E-mail Address:</label>
-                        <input
-                          type="email"
-                          className="form-control"
-                          name="emailAddress"
-                          value={this.state.emailAddress}
-                          onChange={this.handleChange}
-                        />
+                        <input type="email" className="form-control" name="emailAddress" value={this.state.emailAddress} onChange={this.handleChange} />
                       </div>
                       <div className="form-group">
                         <label htmlFor="password">Password:</label>
-                        <input
-                          type="password"
-                          className="form-control"
-                          name="password"
-                          value={this.state.password}
-                          onChange={this.handleChange}
-                        />
+                        <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleChange} />
                       </div>
                     </>
                   )}
                   {this.state.otpRequired && (
                     <div className="form-group">
                       <label htmlFor="otpToken">Multi-Factor Authentication Code:</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        name="otpToken"
-                        value={this.state.otpToken}
-                        onChange={this.handleChange}
-                      />
-                      {this.state.backupCodeSent && (
-                        <div>
-                          A backup code has been sent to your email address.
-                        </div>
-                      )}
+                      <input type="text" className="form-control" name="otpToken" value={this.state.otpToken} onChange={this.handleChange} />
+                      {this.state.backupCodeSent && <div>A backup code has been sent to your email address.</div>}
                       <Mutation mutation={SEND_BACKUP_CODE_MUTATION} variables={{ emailAddress: this.state.emailAddress }}>
                         {(sendBackupCode, { loading }) => (
-                          <button
-                            className='btn btn-link pl-0'
-                            disabled={loading}
-                            onClick={() => this.onSendBackupCode(sendBackupCode)}
-                          >
-                            {this.state.backupCodeSent ? 'Send new backup code' : 'I don\'t have access to my authenticator right now'}
+                          <button className="btn btn-link pl-0" disabled={loading} onClick={() => this.onSendBackupCode(sendBackupCode)}>
+                            {this.state.backupCodeSent ? 'Send new backup code' : "I don't have access to my authenticator right now"}
                           </button>
                         )}
                       </Mutation>
-
                     </div>
                   )}
-                  {this.state.message && (<div className="alert alert-info">{this.state.message}</div>)}
-                  {error && error.message && (<div className="alert alert-danger">{error.message}</div>)}
+                  {this.state.message && <div className="alert alert-info">{this.state.message}</div>}
+                  {error && error.message && <div className="alert alert-danger">{error.message}</div>}
                 </div>
               </div>
             </div>
@@ -171,11 +140,7 @@ class LoginForm extends PureComponent {
                 </a>
               </div>
               <div className="btn-toolbar float-right">
-                <button
-                  className="btn btn-outline-info"
-                  disabled={loading}
-                  onClick={() => this.attemptLogin(login)}
-                >
+                <button type="submit" className="btn btn-outline-info" disabled={loading} onClick={() => this.attemptLogin(login)}>
                   <i className="fal fa-sign-in" /> Login
                 </button>
               </div>
@@ -191,11 +156,11 @@ LoginForm.propTypes = {
   client: PropTypes.shape({
     client_id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    logoImageUrl: PropTypes.string.isRequired,
+    logoImageUrl: PropTypes.string.isRequired
   }).isRequired,
   responseType: PropTypes.string.isRequired,
   redirectUri: PropTypes.string.isRequired,
-  state: PropTypes.string,
+  state: PropTypes.string
 }
 
 export default LoginForm
