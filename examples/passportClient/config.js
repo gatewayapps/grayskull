38551d8b3182023432be12b626c0e5376546db87
Justin Neff <justin@gatewayapps.com>
2019-01-09 11:03:43 -0600

    Update passportClient example and added missing dependency

--- examples/passportClient/config.js
+++ examples/passportClient/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   authServerBaseUrl: process.env.AUTH_SERVER_BASE_URL || 'http://localhost:3000',
   clientBaseUrl: process.env.CLIENT_BASE_URL || 'http://localhost:5001',
-  clientId: process.env.CLIENT_ID || 2,
-  secret: process.env.CLIENT_SECRET || 'e2ded7392cbc8735aa4c7b4950f0909fcd5a8483e357d3d00d95f8767f3e7f6e'
+  clientId: process.env.CLIENT_ID || '7911d298-0698-40fb-bb5e-2bf695f45575',
+  secret: process.env.CLIENT_SECRET || '5b7ff940c3203dc993c3fb9bcc05a2940bc5e744e45bbc1517cf533ab5aa2d4b'
 }
