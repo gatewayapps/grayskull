8acdd6467d8e592864af45230ee109d4676652a1
Daniel Gary <daniel@gatewayapps.com>
2019-01-17 09:45:09 -0600

    show a modal for 45 seconds, then redirect to home route

--- public/pages/oobe/index.js
+++ public/pages/oobe/index.js
@@ -3,12 +3,37 @@ import Primary from '../../layouts/primary'
 import { GettingStarted } from './GettingStarted'
 import { ServerConfiguration } from './ServerConfiguration'
 import { DatabaseConfiguration } from './DatabaseConfiguration'
+import { MailConfiguration } from './MailConfiguration'
+import { SecurityConfiguration } from './SecurityConfiguration'
+import { ConfirmConfiguration } from './ConfirmConfiguration'
+import { Modal, ModalBody } from 'reactstrap'
+
+import gql from 'graphql-tag'
+import { ApolloConsumer } from 'react-apollo'
+
+const SAVE_CONFIGURATION = gql`
+  mutation SAVE_CONFIGURATION($configuration: SaveConfigurationArgs!) {
+    saveConfiguration(data: $configuration) {
+      success
+      error
+      message
+    }
+  }
+`
+
 export default class OobeIndex extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      saving: false,
       currentStep: 0,
       steps: [
+        {
+          valid: false
+        },
+        {
+          valid: false
+        },
         {
           valid: false
         },
@@ -29,12 +54,28 @@ export default class OobeIndex extends React.Component {
         },
         Database: {
           provider: 'postgres',
-          adminUsername: '',
-          adminPassword: '',
-          serverAddress: '',
+          adminUsername: 'root',
+          adminPassword: 'pass',
+          serverAddress: '127.0.0.1',
           serverPort: '5432',
-          databaseName: '',
+          databaseName: 'grayskull',
           connectionVerified: false
+        },
+        Mail: {
+          serverAddress: '127.0.0.1',
+          port: '25',
+          username: '',
+          password: '',
+          fromAddress: 'admin@grayskull.io'
+        },
+        Security: {
+          passwordRequiresLowercase: false,
+          passwordRequiresUppercase: false,
+          passwordRequiresNumber: false,
+          passwordRequiresSymbol: false,
+          passwordMinimumLength: '8',
+          multifactorRequired: false,
+          accessTokenExpirationSeconds: 1800
         }
       }
     }
@@ -73,6 +114,37 @@ export default class OobeIndex extends React.Component {
           />
         )
       }
+      case 3: {
+        return (
+          <MailConfiguration
+            stepIndex={3}
+            onConfigurationChanged={(data) => {
+              const config = this.state.configuration
+              config.Mail = data
+              this.setState({ configuration: config })
+            }}
+            data={this.state.configuration.Mail}
+            onValidationChanged={this.onValidationChanged}
+          />
+        )
+      }
+      case 4: {
+        return (
+          <SecurityConfiguration
+            stepIndex={4}
+            onConfigurationChanged={(data) => {
+              const config = this.state.configuration
+              config.Security = data
+              this.setState({ configuration: config })
+            }}
+            data={this.state.configuration.Security}
+            onValidationChanged={this.onValidationChanged}
+          />
+        )
+      }
+      case 5: {
+        return <ConfirmConfiguration stepIndex={5} configuration={this.state.configuration} onValidationChanged={this.onValidationChanged} />
+      }
     }
   }
 
@@ -83,6 +155,18 @@ export default class OobeIndex extends React.Component {
     this.setState({
       steps
     })
+  }
+
+  waitForRedirect = async () => {
+    let secondsTicked = 0
+    const redirectInterval = window.setInterval(() => {
+      secondsTicked++
+      this.setState({ secondsRemaining: 45 - secondsTicked })
+      if (secondsTicked >= 45) {
+        window.clearInterval(redirectInterval)
+        window.location = `${this.state.configuration.Server.baseUrl}`
+      }
+    }, 1000)
   }
 
   render() {
@@ -96,43 +180,79 @@ export default class OobeIndex extends React.Component {
               </h4>
               <div className="row justify-content-center ">
                 <div className="col-sm-12 col-md-8 col-push-2">
-                  <form
-                    autoComplete="nope"
-                    onSubmit={(e) => {
-                      e.preventDefault()
-                    }}>
-                    <input autoComplete="false" name="hidden" type="text" className="d-none" />
-                    <div className="card">
-                      <div className="card-body">{this.renderCardBody()}</div>
-                      <div className="card-footer">
-                        <div className="btn-toolbar float-right">
-                          {this.state.currentStep > 0 && (
-                            <button
-                              type="button"
-                              onClick={() => {
-                                this.setState({ currentStep: this.state.currentStep - 1 })
-                              }}
-                              className="btn btn-outline-default mr-2">
-                              Back
-                            </button>
-                          )}
-                          <button
-                            type="submit"
-                            onClick={() => {
-                              this.setState({ currentStep: this.state.currentStep + 1 })
-                            }}
-                            className="btn btn-outline-success"
-                            disabled={!this.state.steps[this.state.currentStep].valid}>
-                            Next
-                          </button>
+                  <ApolloConsumer>
+                    {(apolloClient) => (
+                      <form
+                        autoComplete="nope"
+                        onSubmit={(e) => {
+                          e.preventDefault()
+                        }}>
+                        <input autoComplete="false" name="hidden" type="text" className="d-none" />
+                        <div className="card">
+                          <div className="card-body">{this.renderCardBody()}</div>
+                          <div className="card-footer">
+                            <div className="btn-toolbar float-right">
+                              {this.state.currentStep > 0 && (
+                                <button
+                                  type="button"
+                                  onClick={() => {
+                                    this.setState({ currentStep: this.state.currentStep - 1 })
+                                  }}
+                                  className="btn btn-outline-default mr-2">
+                                  Back
+                                </button>
+                              )}
+                              {this.state.currentStep < this.state.steps.length - 1 && (
+                                <button
+                                  type="submit"
+                                  onClick={() => {
+                                    this.setState({ currentStep: this.state.currentStep + 1 })
+                                  }}
+                                  className="btn btn-outline-success"
+                                  disabled={!this.state.steps[this.state.currentStep].valid}>
+                                  Next
+                                </button>
+                              )}
+                              {this.state.currentStep === this.state.steps.length - 1 && (
+                                <button
+                                  type="submit"
+                                  disabled={this.state.saving}
+                                  onClick={async () => {
+                                    this.setState({ saving: true, secondsRemaining: 45 })
+                                    this.waitForRedirect()
+
+                                    const config = this.state.configuration
+                                    delete config.Database.connectionError
+                                    delete config.Database.connectionVerified
+                                    delete config.Database.verifyingConnection
+
+                                    config.Database.serverPort = parseInt(config.Database.serverPort)
+                                    config.Mail.port = parseInt(config.Mail.port)
+                                    config.Security.passwordMinimumLength = parseInt(config.Security.passwordMinimumLength)
+
+                                    const { data } = await apolloClient.mutate({ mutation: SAVE_CONFIGURATION, variables: { configuration: config } })
+                                  }}
+                                  className="btn btn-success">
+                                  Save
+                                </button>
+                              )}
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  </form>
+                      </form>
+                    )}
+                  </ApolloConsumer>
                 </div>
               </div>
             </div>
           </div>
+          {this.state.saving && (
+            <Modal isOpen>
+              <ModalBody>
+                Server is restarting. You will be redirected to the admin registration in <b>{this.state.secondsRemaining}</b> seconds.
+              </ModalBody>
+            </Modal>
+          )}
         </div>
       </Primary>
     )
