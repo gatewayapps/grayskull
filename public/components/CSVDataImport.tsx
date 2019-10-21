5c5923f2984452bf15869791f0a88a01d0cab34c
NathanWaddell121107 <nathan.waddell@gatewayapps.com>
2019-10-21 08:38:54 -0500

    Added UX improvements giving users visibility to what is successfully imported, fixed typescript warning

--- public/components/CSVDataImport.tsx
+++ public/components/CSVDataImport.tsx
@@ -3,6 +3,9 @@ import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
 import Permissions from '../utils/permissions'
 import { CSVReader } from 'react-papaparse'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+import { faCircleNotch, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 const CREATE_USER_MUTATION = gql`
   mutation CREATE_USER_MUTATION($firstName: String!, $lastName: String!, $displayName: String, $gender: String, $birthday: Date, $profileImageUrl: String, $permissions: Int!, $emailAddress: String!) {
@@ -43,12 +46,14 @@ export interface CSVDataImportState {
   importedCSVData: any
   errorMessage: string
   currentlyImportingUsers: boolean
+  successCount: any
+  failedImportArray: any
+  fileInput: any
 }
 
 export default class CSVDataImport extends React.Component<CSVDataImportProps, CSVDataImportState> {
   constructor(props: CSVDataImportProps) {
     super(props)
-    this.fileInput = React.createRef()
 
     this.state = {
       modifiedState: { permissions: props.user.permissions },
@@ -58,8 +63,44 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
       valid: false,
       importedCSVData: undefined,
       errorMessage: '',
-      currentlyImportingUsers: false
+      currentlyImportingUsers: false,
+      successCount: [],
+      failedImportArray: [],
+      fileInput: React.createRef()
     }
+  }
+
+  renderCSVUsersImportList = () => {
+    return this.state.importedCSVData.data.map((item, i) => {
+      let importUserStatusIcon
+      let iconClassName
+      if (this.state.currentlyImportingUsers) {
+        importUserStatusIcon = faCircleNotch
+        iconClassName = 'fa-spin text-warning'
+        if (this.state.successCount.length >= i) {
+          importUserStatusIcon = faCheckCircle
+          iconClassName = 'text-success'
+        } else if (this.state.failedImportArray.length >= i) {
+          importUserStatusIcon = faTimesCircle
+          iconClassName = 'text-danger'
+        }
+      } else if (!this.state.currentlyImportingUsers && this.state.successCount.length >= i) {
+        importUserStatusIcon = faCheckCircle
+        iconClassName = 'text-success'
+      } else {
+        importUserStatusIcon = faCircle
+        iconClassName = 'text-muted'
+      }
+
+      if (item[0] !== 'Email') {
+        return (
+          <div style={{ margin: '10px 0', display: 'flex' }} key={i}>
+            <FontAwesomeIcon icon={importUserStatusIcon} className={iconClassName} />
+            <div style={{ marginLeft: '10px' }}>{`${item[0]}, ${item[1]}, ${item[2]}, ${item[3]}`}</div>
+          </div>
+        )
+      }
+    })
   }
 
   handleReadCSV = (data) => {
@@ -73,7 +114,7 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
   }
 
   handleImportOffer = () => {
-    this.fileInput.current.click()
+    this.state.fileInput.current.click()
     this.setState({ errorMessage: '' })
   }
 
@@ -95,8 +136,9 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
         </div>
         <form>
           <div className="form-group" style={{ padding: '20px' }}>
-            Please upload your CSV file in the format of: <br /> Email, First Name, Last Name, Permissions, Gender (optional), Display Name (optional), Birthday (optional) (mm/dd/yyyy)
+            Please upload your CSV file in the following format: <br /> Email, First Name, Last Name, Permissions, Gender (optional), Display Name (optional), Birthday (optional) (mm/dd/yyyy)
           </div>
+          {this.state.importedCSVData && <div style={{ padding: '20px', maxHeight: '350px', overflowY: 'auto' }}>{this.renderCSVUsersImportList()}</div>}
 
           {this.state.loadingMessage && (
             <div className="alert alert-warning" style={{ width: '60%', margin: '20px auto' }}>
@@ -114,7 +156,7 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
             </div>
           )}
         </form>
-        <div className="card-footer w-100">
+        <div style={{ padding: '1.25rem 1.25rem' }} className="card-footer w-100">
           <div className="btn-toolbar">
             {this.state.importing && (
               <div
@@ -128,13 +170,19 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
                   {(createUser: (payload: any) => Promise<any>, { loading }) => {
                     return (
                       <>
-                        <CSVReader onFileLoaded={this.handleReadCSV} inputRef={this.fileInput} style={{ display: 'none' }} onError={this.handleOnError} configOptions={{ skipEmptyLines: 'greedy' }} />
+                        <CSVReader
+                          onFileLoaded={this.handleReadCSV}
+                          inputRef={this.state.fileInput}
+                          style={{ display: 'none' }}
+                          onError={this.handleOnError}
+                          configOptions={{ skipEmptyLines: 'greedy' }}
+                        />
                         <button disabled={this.state.importedCSVData} className="btn btn-outline-success" style={{ width: '40%' }} onClick={this.handleImportOffer}>
                           Upload CSV file
                         </button>
                         <button
                           disabled={this.state.currentlyImportingUsers}
-                          style={{ width: '90%' }}
+                          style={{ width: '90%', justifySelf: 'end' }}
                           className="btn btn-secondary mr-2"
                           onClick={() => {
                             this.setState({ importing: false, modifiedState: {} })
@@ -145,17 +193,18 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
                           <i className="fa fa-fw fa-times" /> Cancel
                         </button>
                         <button
-                          style={{ width: '90%' }}
+                          style={{ width: '90%', justifySelf: 'end' }}
                           disabled={loading || this.state.importedCSVData === undefined}
                           className="btn btn-outline-success"
                           type="button"
                           onClick={async () => {
                             let successCount = []
-                            let successfulImports
-                            let TotalUsersToImport
-                            this.setState({ loadingMessage: 'Please wait while we import your users' })
+                            let totalUsersToImport
+                            let failedImportToStateArray = []
                             if (this.state.importedCSVData) {
                               const CSVData = this.state.importedCSVData.data
+                              totalUsersToImport = CSVData.length - 1
+                              this.setState({ loadingMessage: `Please wait while we import your users. ${successCount.length} of ${totalUsersToImport} users imported` })
 
                               for (let i = 0; i < CSVData.length; i++) {
                                 const item = CSVData[i]
@@ -174,28 +223,34 @@ export default class CSVDataImport extends React.Component<CSVDataImportProps, C
                                     const result = await createUser({ variables: payload })
 
                                     successCount.push(result.data.createUser.success)
-                                    successfulImports = successCount.length
-                                    TotalUsersToImport = CSVData.length - 1
 
-                                    if (i === TotalUsersToImport) {
-                                      this.setState({ currentlyImportingUsers: false })
-                                    }
+                                    this.setState({
+                                      successCount: successCount,
+                                      loadingMessage: `Please wait while we import your users. ${successCount.length} of ${totalUsersToImport} users imported`
+                                    })
 
-                                    if (successCount.length === TotalUsersToImport) {
+                                    if (successCount.length === totalUsersToImport) {
                                       this.setState({
-                                        successMessage: `Users have been successfully imported! - Number of users imported successfully: (${successfulImports} out of ${TotalUsersToImport})`,
-                                        loadingMessage: ''
+                                        successMessage: `Users have been successfully imported!`,
+                                        loadingMessage: '',
+                                        currentlyImportingUsers: false
                                       })
                                       setTimeout(() => {
                                         this.props.onSave()
                                       }, 2000)
                                     }
                                   } catch (err) {
+                                    if (err) {
+                                      failedImportToStateArray.push(false)
+                                    }
                                     let errMessage = err.message.substring(err.message.indexOf(':') + 1)
                                     this.setState({
-                                      errorMessage: `${errMessage} - Number of users imported successfully: (${successfulImports} out of ${TotalUsersToImport})`,
-                                      loadingMessage: ''
+                                      errorMessage: `${errMessage}`,
+                                      loadingMessage: '',
+                                      failedImportArray: failedImportToStateArray,
+                                      currentlyImportingUsers: false
                                     })
+                                    this.props.refetch()
                                   }
                                 }
                               }
