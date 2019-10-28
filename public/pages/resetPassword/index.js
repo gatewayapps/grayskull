3d91774785cc5d2c8d161c17beea9f42828dfe03
Daniel Gary <daniel@gatewayapps.com>
2019-10-28 13:37:09 -0500

    136/maintain email reset password (#137)
    * added current email address to link to resetPassword
    
    * useState, set props.query.emailAddress to emailAddress if present

--- public/pages/resetPassword/index.js
+++ public/pages/resetPassword/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Primary from '../../layouts/primary'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
@@ -11,82 +11,88 @@ const RESET_PASSWORD = gql`
   }
 `
 
-class Login extends React.PureComponent {
-  state = {
-    fingerprint: '',
-    emailAddress: '',
-    emailSent: false
-  }
+const Login = (props) => {
+  const defaultEmailAddress = props.query && props.query.emailAddress ? props.query.emailAddress : ''
 
-  render() {
-    const { props } = this
+  const [fingerprint, setFingerprint] = useState('')
+  const [emailAddress, setEmailAddress] = useState(defaultEmailAddress)
+  const [emailSent, setEmailSent] = useState(false)
 
-    return (
-      <Primary>
-        <BackgroundCover>
-          <div className="container">
-            <Mutation mutation={RESET_PASSWORD} variables={{ emailAddress: this.state.emailAddress }}>
-              {(resetPassword, { loading }) => {
-                return (
-                  <ResponsiveForm
-                    formHeader={<span>Reset your password</span>}
-                    formBody={
-                      <div>
-                        <div className="row">
-                          <div className="col-12">
-                            <h6 className="card-subtitle mb-2 text-muted">Enter your email address and we will send you instructions for resetting your password.</h6>
+  return (
+    <Primary>
+      <BackgroundCover>
+        <div className="container">
+          <Mutation mutation={RESET_PASSWORD} variables={{ emailAddress: emailAddress }}>
+            {(resetPassword, { loading }) => {
+              return (
+                <ResponsiveForm
+                  formHeader={<span>Reset your password</span>}
+                  formBody={
+                    <div>
+                      <div className="row">
+                        <div className="col-12">
+                          <h6 className="card-subtitle mb-2 text-muted">Enter your email address and we will send you instructions for resetting your password.</h6>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-12">
+                          <div className="form-group">
+                            <input
+                              placeholder="E-mail address"
+                              value={emailAddress}
+                              type="email"
+                              onChange={(e) => {
+                                setEmailAddress(e.target.value)
+                                setEmailSent(false)
+                              }}
+                              className="form-control"
+                              name="emailAddress"
+                            />
                           </div>
                         </div>
+                      </div>
 
+                      {emailSent && (
                         <div className="row">
                           <div className="col-12">
-                            <div className="form-group">
-                              <input
-                                placeholder="E-mail address"
-                                type="email"
-                                onChange={(e) => {
-                                  this.setState({ emailAddress: e.target.value, emailSent: false })
-                                }}
-                                className="form-control"
-                                name="emailAddress"
-                              />
-                            </div>
+                            <div className="alert alert-primary mx-4">An e-mail with instructions has been sent to {emailAddress}</div>
                           </div>
                         </div>
+                      )}
+                    </div>
+                  }
+                  formFooter={
+                    <div className="btn-toolbar float-right">
+                      <button
+                        disabled={loading || emailSent}
+                        className="btn btn-outline-primary"
+                        type="button"
+                        onClick={async () => {
+                          const result = await resetPassword()
+                          setEmailSent(true)
+                        }}>
+                        {loading ? <i className="fa fa-spin fa-fw fa-spinner" /> : <i className="fa fa-fw fa-history" />}
+                        Reset Password
+                      </button>
+                    </div>
+                  }
+                />
+              )
+            }}
+          </Mutation>
+        </div>
+      </BackgroundCover>
+    </Primary>
+  )
+}
 
-                        {this.state.emailSent && (
-                          <div className="row">
-                            <div className="col-12">
-                              <div className="alert alert-primary mx-4">An e-mail with instructions has been sent to {this.state.emailAddress}</div>
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                    }
-                    formFooter={
-                      <div className="btn-toolbar float-right">
-                        <button
-                          disabled={loading || this.state.emailSent}
-                          className="btn btn-outline-primary"
-                          type="button"
-                          onClick={async () => {
-                            const result = await resetPassword()
-                            this.setState({ emailSent: true })
-                          }}>
-                          {loading ? <i className="fa fa-spin fa-fw fa-spinner" /> : <i className="fa fa-fw fa-history" />}
-                          Reset Password
-                        </button>
-                      </div>
-                    }
-                  />
-                )
-              }}
-            </Mutation>
-          </div>
-        </BackgroundCover>
-      </Primary>
-    )
+Login.getInitialProps = async ({ req, query, params }) => {
+  if (req) {
+    return { query: req.query, params: req.params }
   }
+
+  return { query, params }
 }
 
 export default Login
