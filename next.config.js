10e4894ef88ce9dccceaad04488415f39b2cdfed
Daniel Gary <daniel@gatewayapps.com>
2019-10-28 15:15:04 -0500

    upgraded to next 9, removed @zeit/with-typescript

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withSass = require('@zeit/next-sass')
 const withCss = require('@zeit/next-css')
-const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript(withSass(withCss()))
+module.exports = withSass(withCss())
