1325372a9bedf7243ab1b1c487460240b855daa0
Daniel Gary <daniel@gatewayapps.com>
2019-10-30 08:12:48 -0500

    rebased on master

--- cradle.config.js
+++ cradle.config.js
@@ -3,11 +3,11 @@ const cradle = require('@gatewayapps/cradle')
 const loaderOptions = new cradle.LoaderOptions(
   'spec',
   {
-    source: './cradle/cradle.yml'
+    source: './.cradle/cradle.yml'
   },
   console
 )
 
-const emitterOptions = require('./cradle/emitters')
+const emitterOptions = require('./.cradle/emitters')
 
 module.exports = new cradle.CradleConfig(loaderOptions, emitterOptions)
