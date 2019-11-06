61cd978026f9372c857b7528fc897f81879d839e
Daniel Gary <daniel@gatewayapps.com>
2019-11-06 15:55:35 -0600

    fixed styled-components for SSR

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,19 @@ import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  // static getInitialProps({ renderPage }) {
-  //   const sheet = new ServerStyleSheet()
-  //   const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
-  //   const styleTags = sheet.getStyleElement()
-  //   return { ...page, styleTags }
-  // }
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet()
+
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement()
+
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags }
+  }
 
   render() {
     return (
@@ -22,6 +29,7 @@ export default class MyDocument extends Document {
 
           <link rel="stylesheet" href="/fontawesome-pro/css/all.min.css" />
           <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+          {this.props.styleTags}
         </Head>
         <body className="custom_class">
           <Main />
