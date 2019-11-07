b4b63d0e066bedc2d474aad70919aa65901d71e5
Daniel Gary <daniel@gatewayapps.com>
2019-11-07 11:36:48 -0600

    added active class to ActiveLink

--- client/components/ActiveLink.js
+++ client/components/ActiveLink.js
@@ -1,12 +1,18 @@
-import Link from 'next/link'
 import { withRouter } from 'next/router'
-import { Children } from 'react'
-import React from 'react'
+import Link from 'next/link'
+import React, { Children } from 'react'
 
-export default withRouter(({ router, children, as, href, ...rest }) => (
-  <Link {...rest} href={href} as={as}>
-    {React.cloneElement(Children.only(children), {
-      className: router.asPath === href || router.asPath === as ? `${children.props.className} active` : children.props.className
-    })}
-  </Link>
-))
+const ActiveLink = ({ router, children, ...props }) => {
+  const child = Children.only(children)
+
+  let className = child.props.className || ''
+  if (router.pathname === props.href) {
+    className = `${className} active`.trim()
+  }
+
+  delete props.activeClassName
+
+  return <Link {...props}>{React.cloneElement(child, { className })}</Link>
+}
+
+export default withRouter(ActiveLink)
