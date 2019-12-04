0327d187b6f806d4b979140568ff6b4fd41e9631
Daniel Gary <daniel@gatewayapps.com>
2019-12-04 10:38:16 -0600

    use nextapirequest instead of incomingmessage

--- pages/api/initialize.tsx
+++ pages/api/initialize.tsx
@@ -2,8 +2,9 @@ import { getCurrentConfiguration } from '../../server/config/ConfigurationManage
 import { buildContext } from '../../server/utils/authentication'
 import UserAccountRepository from '../../server/data/repositories/UserAccountRepository'
 import { getContext } from '../../server/data/context'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   await getContext()
   const { requestContext, responseContext } = await buildContext(req, res)
 
--- pages/api/token.tsx
+++ pages/api/token.tsx
@@ -1,30 +1,68 @@
 import AuthenticationService from '../../server/api/services/AuthenticationService'
 import { buildContext } from '../../server/utils/authentication'
 import { getContext } from '../../server/data/context'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req, res) => {
+const getClientCredentialsFromRequest = (
+  req: NextApiRequest
+): { client_id: string; client_secret: string } | undefined => {
+  if (req.headers.authorization) {
+    const b64 = req.headers.authorization.replace('Basic ', '')
+    const buf = Buffer.from(b64, 'base64')
+    const stringContents = buf.toString('utf8')
+    const authParts = stringContents.split(':')
+    return {
+      client_id: authParts[0],
+      client_secret: authParts[1]
+    }
+  } else {
+    if (req.body.client_id && req.body.client_secret) {
+      return {
+        client_id: req.body.client_id,
+        client_secret: req.body.client_secret
+      }
+    } else {
+      return undefined
+    }
+  }
+}
+
+const postAccessToken = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    res.status(405).json({ success: false, message: 'Method not allowed' })
+    return
+  }
   try {
     await getContext()
     const { requestContext, responseContext } = await buildContext(req, res)
-
     const clientCredentials = this.getClientCredentialsFromRequest(req)
-    if (!req.body || !req.body.grant_type || !clientCredentials) {
+
+    if (!req.body) {
+      res.status(400).json({ success: false, message: 'Invalid request body' })
+      return
+    }
+    const body = JSON.parse(req.body)
+
+    if (!body || !body.grant_type || !clientCredentials) {
+      res.statusCode = 400
       res.status(400).json({ success: false, message: 'Invalid request body' })
       return
     }
 
-    const accessTokenRespnse = await AuthenticationService.getAccessToken(
-      req.body.grant_type,
+    const accessTokenResponse = await AuthenticationService.getAccessToken(
+      body.grant_type,
       clientCredentials.client_id,
       clientCredentials.client_secret,
-      req.body.code,
-      req.body.refresh_token,
+      body.code,
+      body.refresh_token,
       {
-        userContext: req.user || null
+        userContext: requestContext.user || null
       }
     )
-    res.json(accessTokenRespnse)
+    res.json(accessTokenResponse)
   } catch (err) {
     res.status(400).json({ success: false, message: err.message })
   }
 }
+
+export default postAccessToken
