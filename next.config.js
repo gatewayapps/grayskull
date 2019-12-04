0632205410e2f3f8f468d341aa9236ecac130416
Daniel Gary <daniel@gatewayapps.com>
2019-12-04 16:10:36 -0600

    upgrade to next 9.1.4 for custom routing.  added route for token

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const withCss = require('@zeit/next-css')
 
 module.exports = withSass(
   withCss({
+    experimental: {
+      async rewrites() {
+        return [{ source: '/token', destination: '/api/token' }]
+      }
+    },
     serverRuntimeConfig: {
       PROJECT_ROOT: __dirname
     }
