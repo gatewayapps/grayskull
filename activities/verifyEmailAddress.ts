5ce8efd2cdfddf5b0885303c69331417674edcf9
Daniel Gary <daniel@gatewayapps.com>
2020-02-18 10:33:15 -0600

    fixed more bugs with email verification (#191)

--- activities/verifyEmailAddress.ts
+++ activities/verifyEmailAddress.ts
@@ -3,10 +3,12 @@ import { IRequestContext } from '../foundation/context/prepareContext'
 import { GrayskullError, GrayskullErrorCode } from '../foundation/errors/GrayskullError'
 import { setEmailAddressVerified } from '../operations/data/emailAddress/setEmailAddressVerified'
 import { verifyEmailAddressVerificationCode } from '../operations/data/emailAddress/verifyEmailAddressVerificationCode'
+import { clearValue } from '../operations/data/persistentCache/clearValue'
 
 export async function verifyEmailAddress(emailAddress: string, verificationCode: string, context: IRequestContext) {
   if (await verifyEmailAddressVerificationCode(emailAddress, verificationCode, context.dataContext)) {
     await setEmailAddressVerified(emailAddress, context.dataContext)
+    await clearValue(`BACKUP_MFA:${emailAddress}`, context.dataContext)
   } else {
     throw new GrayskullError(
       GrayskullErrorCode.InvalidEmailVerificationCode,
