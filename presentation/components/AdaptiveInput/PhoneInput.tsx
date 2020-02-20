5a68b1dd5dd8acc8768bbd4c3df68acda033d23c
Daniel Gary <danielgary@gmail.com>
2020-02-20 13:43:12 -0600

    added google-libphonenumber, moved components around, added PhoneInput

--- presentation/components/AdaptiveInput/PhoneInput.tsx
+++ presentation/components/AdaptiveInput/PhoneInput.tsx
@@ -4,31 +4,31 @@ import * as libphonenumber from 'google-libphonenumber'
 const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance()
 
 export const PhoneInput: React.FC<AdaptiveInputProps> = ({ className, ...props }) => {
-  let parsedValue = props.value
-  let formattedValue = props.value
-  try {
-    parsedValue = phoneUtil.parse(props.value, 'US')
-    formattedValue = phoneUtil.format(parsedValue, libphonenumber.PhoneNumberFormat.INTERNATIONAL)
-  } catch {}
-  return (
-    <input
-      className={`form-control ${className || ''}`}
-      {...props}
-      onChange={(e) => {
-        let parsedNumber = e.target.value
-        try {
-          const parsed = phoneUtil.parse(parsedNumber, 'US')
-          parsedNumber = parsed.rawInput()
-        } catch {}
+	let parsedValue = props.value
+	let formattedValue = props.value
+	try {
+		parsedValue = phoneUtil.parse(props.value, 'US')
+		formattedValue = phoneUtil.format(parsedValue, libphonenumber.PhoneNumberFormat.INTERNATIONAL)
+	} catch {}
+	return (
+		<input
+			className={`form-control ${className || ''}`}
+			{...props}
+			onChange={(e) => {
+				let parsedNumber = e.target.value
+				try {
+					const parsed = phoneUtil.parse(parsedNumber, 'US')
+					parsedNumber = parsed.rawInput()
+				} catch {}
 
-        props.onChange({
-          target: {
-            name: props.name,
-            value: parsedNumber
-          }
-        })
-      }}
-      value={formattedValue || undefined}
-    />
-  )
+				props.onChange({
+					target: {
+						name: props.name,
+						value: parsedNumber
+					}
+				})
+			}}
+			value={formattedValue || undefined}
+		/>
+	)
 }
