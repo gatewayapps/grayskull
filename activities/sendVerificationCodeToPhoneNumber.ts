7c8101b89e42679a1bbb6334f78ac96b287b1ccb
Daniel Gary <danielgary@gmail.com>
2020-02-20 15:25:02 -0600

    working on adding phone input components

--- activities/sendVerificationCodeToPhoneNumber.ts
+++
@@ -1,8 +0,0 @@
-import { IRequestContext } from '../foundation/context/prepareContext'
-import { ensureAuthenticated } from '../operations/logic/ensureAuthenticated'
-import { generatePhoneNumberVerificationCode } from '../operations/data/phoneNumber/generatePhoneNumberVerificationCode'
-
-export async function sendVerificationCodeToPhoneNumber(phoneNumber: string, context: IRequestContext) {
-  ensureAuthenticated(context)
-  const code = await generatePhoneNumberVerificationCode(phoneNumber, 300, context.dataContext)
-}
