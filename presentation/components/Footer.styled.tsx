92b368455667b8c301fff874bf3b4f3d4b8eb7df
Tyler Broadway <32829986+tbroadway96@users.noreply.github.com>
2020-02-20 18:07:25 -0600

    204 about footer (#206)
    * Created a footer that displays Grayskull version & button that displays a modal w/ release notes
    
    * Small styling changes
    
    * Removed version from modal header, as it shows all previous versions

---
+++ presentation/components/Footer.styled.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 5em;
+  background: #eee;
+  padding: 30px;
+`
+
+export const StyledFooterContent = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 60%;
+  margin-right: -18px;
+`
