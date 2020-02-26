627a84e9385c237f764e231b0cb9bbfea9d5a9f6
Daniel Gary <daniel@gatewayapps.com>
2020-02-25 20:36:43 -0500

    do not attempt to get pinned clients if user is not signed in

--- pages/api/initialize.tsx
+++ pages/api/initialize.tsx
@@ -12,7 +12,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   configuration.Mail.password = PASSWORD_PLACEHOLDER
   configuration.Mail.sendgridApiKey = PASSWORD_PLACEHOLDER
 
-  const pinnedClients = await getPinnedClientsActivity(context)
+  const pinnedClients = context.user ? await getPinnedClientsActivity(context) : []
 
   configuration.HeaderItems = pinnedClients
   const needsConfiguration = !configuration.Server?.baseUrl
@@ -25,18 +25,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     needsAdmin,
     user: context.user
       ? {
-          userAccountId: context.user.userAccountId,
-          firstName: context.user.firstName,
-          lastName: context.user.lastName,
-          gender: context.user.gender,
-          birthday: context.user.birthday,
-          displayName: context.user.displayName,
-          lastPasswordChange: context.user.lastPasswordChange,
-          profileImageUrl: context.user.profileImageUrl,
-          emailAddress: context.user.emailAddress,
-          permissions: context.user.permissions,
-          otpEnabled: context.user.otpEnabled
-        }
+        userAccountId: context.user.userAccountId,
+        firstName: context.user.firstName,
+        lastName: context.user.lastName,
+        gender: context.user.gender,
+        birthday: context.user.birthday,
+        displayName: context.user.displayName,
+        lastPasswordChange: context.user.lastPasswordChange,
+        profileImageUrl: context.user.profileImageUrl,
+        emailAddress: context.user.emailAddress,
+        permissions: context.user.permissions,
+        otpEnabled: context.user.otpEnabled
+      }
       : undefined
   })
 }
