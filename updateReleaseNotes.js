b06fde6a2e33fd93dc7d04adfc8003daf29cfe91
Daniel Gary <daniel@gatewayapps.com>
2020-02-26 13:15:27 -0500

    added update-release-notes to package.json scripts and release.yml workflow (#213)

--- updateReleaseNotes.js
+++ updateReleaseNotes.js
@@ -2,5 +2,5 @@ const cp = require('child_process')
 const packageInfo = require('./package.json')
 
 const RELASE_NOTES_URL = `https://parcelapp.io/api/projects/5bc65fb88f171e07f26fdd55/notes/format/html?raw=true&version=${packageInfo.version}&scope=major`
-console.log(RELASE_NOTES_URL)
+
 cp.execSync(`curl '${RELASE_NOTES_URL}' > ./public/releaseNotes.html`, { stdio: 'ignore' })
