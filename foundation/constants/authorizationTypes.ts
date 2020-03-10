8d09c9e8624a5fb738729eb77055a9316b77726d
Daniel Gary <danielgary@gmail.com>
2020-03-10 16:48:12 -0500

    created constants for authorization types

---
+++ foundation/constants/authorizationTypes.ts
@@ -0,0 +1,11 @@
+export const GrantTypes = {
+	AuthorizationCode: 'authorization_code',
+	RefreshToken: 'refresh_token',
+	Password: 'password'
+}
+
+export const GrantTypeNames = {
+	[GrantTypes.AuthorizationCode]: 'Authorization Code',
+	[GrantTypes.RefreshToken]: 'Refresh Token',
+	[GrantTypes.Password]: 'Password'
+}
