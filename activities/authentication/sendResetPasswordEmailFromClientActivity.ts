49e9f2e5941e67cf41f2546972004fdad36a3501
Daniel Gary <danielgary@gmail.com>
2020-03-23 16:41:50 -0500

    working on reset password from client

--- activities/authentication/sendResetPasswordEmailFromClientActivity.ts
+++ activities/authentication/sendResetPasswordEmailFromClientActivity.ts
@@ -1,6 +1,25 @@
+import { IRequestContext } from '../../foundation/context/prepareContext'
+import { getUserAccountByEmailAddress } from '../../operations/data/userAccount/getUserAccountByEmailAddress'
+import { getClient } from '../../operations/data/client/getClient'
+import { isValidClientRedirectUri } from '../../operations/logic/isValidClientRedirectUri'
+
 export async function sendResetPasswordEmailFromClientActivity(
 	emailAddress: string,
 	clientId: string,
-	redirectUri: string | undefined,
+	redirectUri: string,
 	context: IRequestContext
-) {}
+) {
+	const userAccount = await getUserAccountByEmailAddress(emailAddress, context.dataContext, context.cacheContext, false)
+	if (!userAccount) {
+		return
+	}
+	const client = await getClient(clientId, context.dataContext, false)
+	if (!client) {
+		return
+	}
+
+	const isValidRedirect = isValidClientRedirectUri(client, redirectUri)
+	if (!isValidRedirect) {
+		return
+	}
+}
