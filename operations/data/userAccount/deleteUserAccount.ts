699e772752eaeda53e4bb6cceb8c8717e3315e7b
Tyler Broadway <tyler.broadway@gatewayapps.com>
2020-06-02 16:02:19 -0500

    Made changes to deleteUserAccount

--- operations/data/userAccount/deleteUserAccount.ts
+++ operations/data/userAccount/deleteUserAccount.ts
@@ -3,11 +3,13 @@ import Knex from 'knex'
 import { IUserAccount } from '../../../foundation/types/types'
 
 export async function deleteUserAccount(userAccountId: string, userContext: UserContext, context: Knex) {
-	return context<IUserAccount>('UserAccounts')
+	const userUpdate = await context<IUserAccount>('UserAccounts')
 		.where({ userAccountId })
 		.update({
 			isActive: false,
 			deletedAt: new Date(),
 			deletedBy: userContext.userAccountId
 		})
+
+	return userUpdate
 }
