477f21b5c4a1f53943e816c65191c2ed0ac9b815
Daniel Gary <daniel@gatewayapps.com>
2020-06-29 15:12:25 -0500

    working on jwks support

---
+++ pages/api/.well-known/openid-configuration.ts
@@ -0,0 +1,30 @@
+import { prepareContext } from '../../../foundation/context/prepareContext'
+import { Scopes } from '../../../foundation/constants/scopes'
+export default async (req, res) => {
+	const context = await prepareContext(req, res)
+
+	const issuer = context.configuration.Server.baseUrl
+	const authorization_endpoint = new URL(`/authorize`, issuer).toString()
+	const token_endpoint = new URL('/token', issuer).toString()
+	const userinfo_endpoint = new URL('/userinfo', issuer).toString()
+	// const revocation_endpoint -- needs to be implemented
+	const jwks_uri = new URL('/.well-known/jwks.json', issuer)
+
+	const response_types_supported = ['code', 'token', 'id_token']
+	const subject_types_supported = ['public']
+
+	const id_token_signing_alg_values_supported = ['RS256', 'HMAC256']
+	const scopes_supported = Scopes.map((s) => s.id)
+
+	res.json({
+		issuer,
+		authorization_endpoint,
+		token_endpoint,
+		userinfo_endpoint,
+		jwks_uri,
+		response_types_supported,
+		subject_types_supported,
+		id_token_signing_alg_values_supported,
+		scopes_supported
+	})
+}
