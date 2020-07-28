60ebe0aad59ef69e66052ee19beab1b55d762222
Daniel Gary <daniel@gatewayapps.com>
2020-07-28 08:44:19 -0500

    expose jwks.json and openid-configuration with cors

--- pages/api/.well-known/jwks.json.ts
+++ pages/api/.well-known/jwks.json.ts
@@ -1,10 +1,29 @@
+import Cors from 'cors'
 import { prepareContext } from '../../../foundation/context/prepareContext'
 import { getRSAPublicKey } from '../../../operations/data/configuration/getRSAPublicKey'
 import { getRSAKeyId } from '../../../operations/data/configuration/getRSAKeyId'
 import { generateRSAKeyPair } from '../../../operations/data/configuration/generateRSAKeyPair'
 import keyto from '@trust/keyto'
 import { NextApiRequest, NextApiResponse } from 'next'
+
+const cors = Cors({
+	methods: ['GET', 'HEAD', 'OPTIONS', 'POST']
+})
+
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
+
+			return resolve(result)
+		})
+	})
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	await runMiddleware(req, res, cors)
 	const context = await prepareContext(req, res)
 	let publicKey = await getRSAPublicKey(context.dataContext)
 	let keyId = await getRSAKeyId(context.dataContext)
