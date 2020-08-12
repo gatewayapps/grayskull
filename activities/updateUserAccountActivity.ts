87508382c95ac97247cf1d455fdd31267ff62b91
NathanWaddell121107 <nathan.waddell@gatewayapps.com>
2020-08-12 13:48:34 -0500

    Adding password update to server updateUser logic

--- activities/updateUserAccountActivity.ts
+++ activities/updateUserAccountActivity.ts
@@ -5,6 +5,7 @@ import { ensureAdministrator } from '../operations/logic/ensureAdministrator'
 import { updateUserAccount } from '../operations/data/userAccount/updateUserAccount'
 import { getAuthorizedUserForClient } from '../operations/data/client/getAuthorizedUserForClient'
 import { createUserContextForUserId } from '../foundation/context/getUserContext'
+import bcrypt from 'bcrypt'
 
 export async function updateUserAccountActivity(
 	userAccountId: string,
@@ -16,9 +17,13 @@ export async function updateUserAccountActivity(
 		ensureAdministrator(context)
 	}
 
+	if (userDetails.password) {
+		userDetails.passwordHash = await bcrypt.hash(userDetails.password, 10)
+		delete userDetails.password
+	}
+
 	await updateUserAccount(userAccountId, userDetails, context.dataContext, context.user, context.cacheContext)
 
-	//
 	if (context.userClient) {
 		return getAuthorizedUserForClient(
 			context.userClient!.userClientId,
