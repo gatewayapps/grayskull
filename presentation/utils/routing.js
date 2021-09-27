3974f6d213557d23752b195c8ae59c0c5c6f4621
Nathan Waddell <45477581+NathanWaddell121107@users.noreply.github.com>
2021-09-27 15:52:09 -0500

    Fix issue with state being in wrong format in /authorize (#313)

--- presentation/utils/routing.js
+++ presentation/utils/routing.js
@@ -12,3 +12,20 @@ export function generateRoutingState({ pathname, query }) {
 export function parseRoutingState(state) {
 	return JSON.parse(atob(state))
 }
+
+export function parseQueryParams(query) {
+	if (typeof query === 'string') return query
+
+	const queryLength = Object.keys(query).length
+	let current = 1
+	let queryString = ''
+	for (const param of Object.keys(query)) {
+		if (param === 'scope') {
+			queryString += `${param}=${encodeURIComponent(query[param])}${current < queryLength ? '&' : ''}`
+		} else {
+			queryString += `${param}=${query[param]}${current < queryLength ? '&' : ''}`
+		}
+		current++
+	}
+	return queryString
+}
