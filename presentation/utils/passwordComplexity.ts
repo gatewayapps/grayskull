1a7faaeea965dd491a16459f3ee245ea2f657b06
unknown <waddellnathan1211@gmail.com>
2021-10-01 23:23:20 -0500

    Convert password complexity utils to typescript, fixed bug with wrong config property

--- presentation/utils/passwordComplexity.ts
+++ presentation/utils/passwordComplexity.ts
@@ -1,30 +1,32 @@
+import { ISecurityConfiguration } from '../../foundation/types/types'
+
 const LOWERCASE_REGEX = /[a-z]/
 const UPPERCASE_REGEX = /[A-Z]/
 const NUMBER_REGEX = /\d/
 const SYMBOL_REGEX = /[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/
 
-export function hasLowercase(password) {
+export function hasLowercase(password: string): boolean {
 	return LOWERCASE_REGEX.test(password)
 }
 
-export function hasUppercase(password) {
+export function hasUppercase(password: string): boolean {
 	return UPPERCASE_REGEX.test(password)
 }
 
-export function hasNumber(password) {
+export function hasNumber(password: string): boolean {
 	return NUMBER_REGEX.test(password)
 }
 
-export function hasSymbol(password) {
+export function hasSymbol(password: string): boolean {
 	return SYMBOL_REGEX.test(password)
 }
 
-export function hasMinLength(password, minLength) {
+export function hasMinLength(password: string, minLength: number): boolean {
 	return typeof password === 'string' && password.length >= minLength
 }
 
-export function validatePassword(password, configuration) {
-	if (configuration.minLength && !hasMinLength(password, configuration.minLength)) {
+export function validatePassword(password: string, configuration: ISecurityConfiguration): boolean {
+	if (configuration.passwordMinimumLength && !hasMinLength(password, configuration.passwordMinimumLength)) {
 		return false
 	}
 	if (configuration.passwordRequiresLowercase && !hasLowercase(password)) {
