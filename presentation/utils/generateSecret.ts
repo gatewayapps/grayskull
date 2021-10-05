87c76b61417a9f70dbc05a3a0a4dd05077ceb7c6
unknown <waddellnathan1211@gmail.com>
2021-10-04 22:20:10 -0500

    Fix a few small issues in the secret method

--- presentation/utils/generateSecret.ts
+++ presentation/utils/generateSecret.ts
@@ -1,7 +1,14 @@
 const SECRET_LENGTH = 32
 
-export default function generateSecret(): string {
+export default function generateSecret(): string | null {
+	if (typeof window === 'undefined') return null
+	const secret: string[] = []
 	const array = new Uint8Array(SECRET_LENGTH)
 	window.crypto.getRandomValues(array)
-	return array.map((x) => parseInt(('00' + x.toString(16)).slice(-2))).join('')
+
+	array.forEach((x) => {
+		secret.push(('00' + x.toString(16)).slice(-2))
+	})
+
+	return secret.join('')
 }
