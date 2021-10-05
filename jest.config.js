f18cb3233462aab740ef29450bcb9fa2f68e5afb
unknown <waddellnathan1211@gmail.com>
2021-10-04 22:19:47 -0500

    Add global crypto support for jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ config()
 
 module.exports = {
 	roots: ['<rootDir>'],
+	setupFiles: ['<rootDir>/tests/settings/jest.crypto-setup.js'],
 	testMatch: ['**/?(*.)(spec|test).ts?(x)'],
 	testPathIgnorePatterns: ['node_modules'],
 	transform: {
