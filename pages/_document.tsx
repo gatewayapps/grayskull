cf0f7685b2fbd638f6e598690307cbc0df70c81f
Daniel Gary <daniel@gatewayapps.com>
2021-10-05 21:29:48 -0500

    refactored document.js to typescript (#339)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,16 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-	static getInitialProps({ renderPage }) {
+export default class MyDocument extends Document<{
+	styleTags: React.ReactElement<{}, string | React.JSXElementConstructor<any>>[]
+}> {
+	static async getInitialProps(ctx: DocumentContext) {
 		// Step 1: Create an instance of ServerStyleSheet
 		const sheet = new ServerStyleSheet()
 
 		// Step 2: Retrieve styles from components in the page
-		const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
+		const page = ctx.renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
 
 		// Step 3: Extract the styles as <style> tags
 		const styleTags = sheet.getStyleElement()
