9754f4f6b02359f37e7a9721d6b05e79d72497a4
Jeff Ratton <jeffreyratton@gmail.com>
2021-10-15 13:40:27 -0500

    wip

--- pages/admin/clients/add.tsx
+++ pages/admin/clients/add.tsx
@@ -2,7 +2,7 @@
 import gql from 'graphql-tag'
 import debounce from 'lodash/debounce'
 import Link from 'next/link'
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { ApolloConsumer, Mutation, Query } from 'react-apollo'
 import uuid from 'uuid/v4'
 import ErrorMessage from '../../../presentation/components/ErrorMessage'
@@ -11,11 +11,10 @@ import generateSecret from '../../../presentation/utils/generateSecret'
 import { ALL_CLIENTS_QUERY } from './index'
 import LoadingIndicator from '../../../presentation/components/LoadingIndicator'
 import ClientForm from '../../../presentation/components/ClientForm'
-import { GrantTypes } from '../../../foundation/constants/grantTypes'
-import { CreateClientOperationArgs } from '../../../foundation/types/types'
+import { GrantType, GrantTypes } from '../../../foundation/constants/grantTypes'
 
 import AuthenticatedRoute from '../../../presentation/layouts/authenticatedRoute'
-import Permissions from '../../../presentation/utils/permissions'
+import { Permissions } from '../../../foundation/constants/permissions'
 
 const CREATE_CLIENT_MUTATION = gql`
 	mutation CREATE_CLIENT_MUTATION($data: CreateClientArgs!) {
@@ -47,8 +46,24 @@ const GET_SCOPES_FOR_CLIENT_QUERY = gql`
 	}
 `
 
-const ClientAddPage: React.FC = () => {
-	const [client, setClient] = React.useState<CreateClientOperationArgs>({
+interface ClientTemplate {
+	client_id: string
+	name: string
+	logoImageUrl: string
+	description: string
+	baseUrl: string
+	homePageUrl: string
+	public: boolean
+	redirectUris: string[]
+	scopes: string
+	AuthorizationFlows: string
+	isActive: boolean
+	TokenSigningMethod: string
+	secret: string | null
+}
+
+const ClientAddPage = () => {
+	const [client, setClient] = React.useState<ClientTemplate>({
 		client_id: uuid(),
 		name: '',
 		logoImageUrl: '/grayskull.png',
@@ -56,154 +71,157 @@ const ClientAddPage: React.FC = () => {
 		baseUrl: '',
 		homePageUrl: '',
 		public: true,
-		redirectUris: '',
+		redirectUris: [],
 		scopes: '',
+		AuthorizationFlows: '',
 		isActive: true,
-		TokenSigningMethod: 'RS256'
+		TokenSigningMethod: 'RS256',
+		secret: null
 	})
-	AuthorizationFlows: [GrantTypes.AuthorizationCode, GrantTypes.RefreshToken],
-	const [customizeClientId, setcustomizeClientId] = React.useState(false)
-	const [clientIdValid, setclientIdValid] = React.useState(true)
-	const [clientFormValid, setclientFormValid] = React.useState(true)
-	const [result, setresult] = React.useState()
+	const [customizeClientId, setCustomizeClientId] = React.useState(false)
+	const [clientIdValid, setClientIdValid] = React.useState(true)
+	const [clientFormValid, setClientFormValid] = React.useState(true)
+	const [result, setResult] = React.useState<any>()
 	const [scopes, setScopes] = React.useState<string[]>([])
+	const [AuthorizationFlows, setAuthorizationFlows] = React.useState<GrantType[]>([
+		GrantTypes.AuthorizationCode,
+		GrantTypes.RefreshToken
+	])
 	const [redirectUris, setRedirectUris] = React.useState<Array<{ key: string; value: '' }>>([])
 
 	const checkClientId = debounce(async (apolloClient) => {
 		if (client.client_id) {
-			this.setState({ clientIdValid: false })
+			setClientIdValid(false)
 			return
 		}
 		const { data } = await apolloClient.query({
 			query: CHECK_CLIENT_ID_QUERY,
-			variables: { client_id: this.state.client.client_id },
+			variables: { client_id: client.client_id },
 			fetchPolicy: 'network-only'
 		})
 		if (data && data.client) {
-			this.setState({ clientIdValid: false })
+			setClientIdValid(false)
 		} else {
-			this.setState({ clientIdValid: true })
+			setClientIdValid(true)
 		}
 	}, 300)
 
-	handleClientIdChange = (evt, apolloClient) => {
-		this.handleChange(evt)
-		this.checkClientId(apolloClient)
-	}
-
-	handleClientFormChange = (name, value) => {
-		this.setState((prevState) => ({
+	const handleClientFormChange = (name, value) => {
+		setClient((prevState) => ({
 			...prevState,
 			client: {
-				...prevState.client,
+				...client,
 				[name]: value
 			}
 		}))
 	}
 
-	handleChange = (evt) => {
+	const handleChange = (evt) => {
 		const { name, value } = evt.target
-
-		this.setState((prevState) => ({
-			...prevState,
-			client: {
-				...prevState.client,
-				[name]: value
-			}
-		}))
+		if (name === 'redirectUris') {
+			setRedirectUris(value)
+		} else if (name === 'scopes') {
+			setScopes(value)
+		} else if (name === 'AuthorizationFlows') {
+			setAuthorizationFlows(value)
+		} else {
+			setClient((prevState) => ({
+				...prevState,
+				client: {
+					...client,
+					[name]: value
+				}
+			}))
+		}
+	}
+	const handleClientIdChange = (evt, apolloClient) => {
+		handleChange(evt)
+		checkClientId(apolloClient)
 	}
 
-	onClientFormValidated = (isValid) => {
-		this.setState({ clientFormValid: isValid })
+	const onClientFormValidated = (isValid) => {
+		setClientFormValid(isValid)
 	}
 
-	submitClient = async (evt, createClient) => {
+	const submitClient = async (evt, createClient) => {
 		evt.preventDefault()
-		if (!this.state.clientIdValid || !this.state.clientFormValid) {
+		if (!clientIdValid || !clientFormValid) {
 			return
 		}
 		const secret = generateSecret()
 
-		const { redirectUris, AuthorizationFlows, scopes, ...data } = this.state.client
-
 		if (redirectUris) {
-			data.redirectUris = JSON.stringify(redirectUris.map((r) => r.value))
+			client.redirectUris = redirectUris.map((r) => r.value)
 		}
 		if (scopes) {
-			data.scopes = JSON.stringify(scopes)
+			client.scopes = JSON.stringify(scopes)
 		}
 		if (AuthorizationFlows) {
-			data.AuthorizationFlows = JSON.stringify(AuthorizationFlows)
+			client.AuthorizationFlows = JSON.stringify(AuthorizationFlows)
 		}
 
-		data.secret = secret
+		client.secret = secret
 
-		const res = await createClient({ variables: { data } })
+		const res = await createClient({ variables: { client } })
 
 		if (res.data && res.data.createClient) {
-			this.setState({
-				result: {
-					client_id: res.data.createClient.client_id,
-					secret
-				}
+			setResult({
+				client_id: res.data.createClient.client_id,
+				secret
 			})
 		}
 	}
 
-	toggleCustomize = () => {
-		this.setState((prevState) => ({
-			...prevState,
-			customizeClientId: !prevState.customizeClientId
-		}))
+	const toggleCustomize = () => {
+		setCustomizeClientId(!customizeClientId)
 	}
 
 	return (
-		<AuthenticatedRoute permission={Permissions.ADMIN}>
+		<AuthenticatedRoute permission={Permissions.Admin}>
 			<Query query={GET_SCOPES_FOR_CLIENT_QUERY}>
 				{(results) => {
-					if (loadingScopes) {
+					if (results.loadingScopes) {
 						return <LoadingIndicator />
 					}
 
-					if (error) {
-						return <ErrorMessage error={error} />
+					if (results.error) {
+						return <ErrorMessage error={results.error} />
 					}
-
 					return (
 						<Mutation mutation={CREATE_CLIENT_MUTATION} refetchQueries={[{ query: ALL_CLIENTS_QUERY }]}>
-							{(createClient, { error, loading }) => (
+							{(createClient, data) => (
 								<div className="container pt-4">
-									<form onSubmit={(e) => this.submitClient(e, createClient)}>
+									<form onSubmit={(e) => submitClient(e, createClient)}>
 										<div className="card">
 											<div className="card-header">Create Client</div>
 											<div className="card-body">
-												<ErrorMessage error={error} />
+												<ErrorMessage error={data.error} />
 												<div className="form-group row">
 													<label className="col-sm-12 col-md-3 col-form-label" htmlFor="client_id">
 														Client ID
 													</label>
 													<div className="col-sm-12 col-md-9">
-														{!this.state.customizeClientId && (
+														{!customizeClientId && (
 															<>
 																<span className="py-2" style={{ verticalAlign: 'middle' }}>
-																	{this.state.client.client_id}
+																	{client.client_id}
 																</span>
-																<button type="button" className="btn btn-link btn-sm" onClick={this.toggleCustomize}>
+																<button type="button" className="btn btn-link btn-sm" onClick={toggleCustomize}>
 																	Customize
 																</button>
 															</>
 														)}
-														{this.state.customizeClientId && (
+														{customizeClientId && (
 															<ApolloConsumer>
 																{(apolloClient) => (
 																	<input
 																		type="text"
-																		className={`form-control ${this.state.clientIdValid ? 'is-valid' : 'is-invalid'}`}
+																		className={`form-control ${clientIdValid ? 'is-valid' : 'is-invalid'}`}
 																		name="client_id"
-																		value={this.state.client.client_id}
-																		onChange={(e) => this.handleClientIdChange(e, apolloClient)}
+																		value={client.client_id}
+																		onChange={(e) => handleClientIdChange(e, apolloClient)}
 																		required
-																		readOnly={this.state.result !== undefined}
+																		readOnly={result !== undefined}
 																		aria-describedby="clientIdHelpBlock"
 																		autoFocus
 																	/>
@@ -217,13 +235,13 @@ const ClientAddPage: React.FC = () => {
 													</div>
 												</div>
 												<ClientForm
-													client={this.state.client}
-													onChange={this.handleClientFormChange}
-													onValidated={this.onClientFormValidated}
-													scopes={data.scopes}
+													client={client}
+													onChange={handleClientFormChange}
+													onValidated={onClientFormValidated}
+													scopes={scopes}
 													grantTypes={data.grantTypes}
 												/>
-												{this.state.result && (
+												{result && (
 													<div className="alert alert-success">
 														<p>
 															Success! Your client_id and client_secret that your application will use to authenticate
@@ -231,15 +249,15 @@ const ClientAddPage: React.FC = () => {
 															client_secret will be visible.
 														</p>
 														<div>
-															<strong>Client Id:</strong> {this.state.result.client_id}
+															<strong>Client Id:</strong> {result.client_id}
 														</div>
 														<div>
-															<strong>Secret:</strong> {this.state.result.secret}
+															<strong>Secret:</strong> {result.secret}
 														</div>
 													</div>
 												)}
 											</div>
-											{!this.state.result && (
+											{result && (
 												<div className="card-footer clearfix">
 													<div className="btn-toolbar float-right">
 														<Link href="/admin/clients">
@@ -250,7 +268,7 @@ const ClientAddPage: React.FC = () => {
 														<button
 															className="btn btn-success"
 															type="submit"
-															disabled={loading || !this.state.clientIdValid || !this.state.clientFormValid}>
+															disabled={!clientIdValid || !clientFormValid}>
 															<i className="fal fa-save" /> Create
 														</button>
 													</div>
