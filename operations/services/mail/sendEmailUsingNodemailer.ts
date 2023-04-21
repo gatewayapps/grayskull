33d2cd1b6063efb34370eb742663310798d7f7b9
Daniel Gary <daniel@gatewayapps.com>
2023-04-21 09:01:50 -0500

    use postmark

--- operations/services/mail/sendEmailUsingNodemailer.ts
+++ operations/services/mail/sendEmailUsingNodemailer.ts
@@ -1,8 +1,6 @@
-import nodemailer, { SendMailOptions, Transport } from 'nodemailer'
-
 import { IMailConfiguration } from '../../../foundation/types/types'
 import { GrayskullError, GrayskullErrorCode } from '../../../foundation/errors/GrayskullError'
-import { userClientHasAllowedScope } from '../../logic/userClientHasAllowedScope'
+import { ServerClient } from 'postmark'
 
 export async function sendEmailUsingNodemailer(
 	to: string,
@@ -24,38 +22,47 @@ export async function sendEmailUsingNodemailer(
 		)
 	}
 
+	const client = new ServerClient(process.env.POSTMARK_API_KEY!)
+	client.sendEmail({
+		From: config.fromAddress,
+		Subject: subject,
+		To: to,
+		HtmlBody: html,
+		TextBody: text,
+		MessageStream: 'outbound'
+	})
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const options: any = {
-		host: config.serverAddress,
-		port: config.port,
-		secure: config.tlsSslRequired,
-		auth: config.username ?? {
-			user: config.username,
-			pass: config.password
-		}
-	}
-
-	console.warn(JSON.stringify({ ...options, auth: { user: config.username, more: config.password } }))
-
-	// if (process.env.NODE_ENV !== 'production') {
-	// 	options.ignoreTLS = true
-	// } else {
-	// 	options.tls = {
-	// 		ciphers: 'SSLv3',
-	// 		rejectUnauthorized: false
+	// const options: any = {
+	// 	host: config.serverAddress,
+	// 	port: config.port,
+	// 	secure: config.tlsSslRequired,
+	// 	auth: config.username ?? {
+	// 		user: config.username,
+	// 		pass: config.password
 	// 	}
 	// }
-	const transport = nodemailer.createTransport(options)
-	await transport.verify()
 
-	console.warn('Transport verified')
-	const messageOptions: SendMailOptions = {
-		from: config.fromAddress,
-		to,
-		subject,
-		text,
-		html
-	}
+	// console.warn(JSON.stringify({ ...options, auth: { user: config.username, more: config.password } }))
 
-	await transport.sendMail(messageOptions)
+	// // if (process.env.NODE_ENV !== 'production') {
+	// // 	options.ignoreTLS = true
+	// // } else {
+	// // 	options.tls = {
+	// // 		ciphers: 'SSLv3',
+	// // 		rejectUnauthorized: false
+	// // 	}
+	// // }
+	// const transport = nodemailer.createTransport(options)
+	// await transport.verify()
+
+	// console.warn('Transport verified')
+	// const messageOptions: SendMailOptions = {
+	// 	from: config.fromAddress,
+	// 	to,
+	// 	subject,
+	// 	text,
+	// 	html
+	// }
+
+	// await transport.sendMail(messageOptions)
 }
