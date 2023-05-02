39a2aa0f2027e66574d064e4b7ae665e2eab8b4a
Hunter West <hunter-alan-08@hotmail.com>
2023-05-02 11:21:41 -0500

    Fix incorrect redirect (#391)
    * fixed incorrect redirecting
    
    * better redirect logic / Can now go back after authorize to the
    
    * fix redirection attempt 2

--- presentation/components/ApplicationInitializer.tsx
+++ presentation/components/ApplicationInitializer.tsx
@@ -1,19 +1,20 @@
-import React, { ReactNode, useState } from 'react'
+import React, { useState } from 'react'
 
-import Router from 'next/router'
 import ConfigurationContext from '../contexts/ConfigurationContext'
 import UserContext from '../contexts/UserContext'
 import LoadingIndicator from './LoadingIndicator'
 
 import { IConfiguration } from '../../foundation/models/IConfiguration'
 import ActivityMessageContainerComponent from './ActivityMessageContainer'
+import Router from 'next/router'
 
 const ApplicationInitializer: React.FC<{ configuration: IConfiguration }> = (props) => {
 	const [response, setResponse] = useState<any>(undefined)
 	const [error, setError] = useState<any>(undefined)
 	const [busy, setBusy] = useState(false)
+	const [redirecting, setRedirecting] = React.useState(false)
 
-	const callInitialize = async () => {
+	const callInitialize = async (redirectTo?: string) => {
 		try {
 			if (busy) {
 				return
@@ -25,7 +26,42 @@ const ApplicationInitializer: React.FC<{ configuration: IConfiguration }> = (pro
 
 			const response = await fetch(finalUrl, { method: 'GET' })
 			const json = await response.json()
+
+			let redirectUri = ''
+			if (json) {
+				if (
+					json.user &&
+					(window.location.pathname === '/login' || window.location.pathname === '/' || window.location.pathname === '')
+				) {
+					redirectUri = '/personal-info'
+				}
+				if (redirectTo) {
+					redirectUri = redirectTo
+				}
+				if (json.needsConfiguration && window.location.pathname !== '/oobe') {
+					redirectUri = '/oobe'
+				}
+				if (!json.needsConfiguration && window.location.pathname.toLowerCase() === '/oobe') {
+					redirectUri = '/'
+				}
+				if (json.needsAdmin && !json.needsConfiguration && window.location.pathname !== '/register') {
+					redirectUri = '/register'
+				}
+				// if (!redirectUri) {
+				// 	if (!response.needsConfiguration && !response.needsAdmin && !response.user && window.location.pathname !== '/login' && window.location.pathname !== '/register') {
+				// 		redirectUri = '/login'
+				// 	}
+				// }
+			}
 			setResponse(json)
+
+			if (redirectUri) {
+				setRedirecting(true)
+
+				await Router.push(redirectUri)
+
+				setRedirecting(false)
+			}
 		} catch (err) {
 			console.error(err)
 			setError(err)
@@ -38,55 +74,22 @@ const ApplicationInitializer: React.FC<{ configuration: IConfiguration }> = (pro
 		callInitialize()
 	}
 
-	let redirectUri = ''
-	let content: ReactNode = props.children
+	const content = React.useMemo(() => {
+		let innerContent: React.ReactNode = null
+		if (error) {
+			innerContent = <div className="alert alert-danger">{error.message}</div>
+		} else if (busy) {
+			innerContent = <LoadingIndicator message="Loading..." />
+		} else if (redirecting) {
+			innerContent = <LoadingIndicator message="Redirecting..." />
+		}
 
-	if (error) {
-		content = (
-			<ActivityMessageContainerComponent>
-				<div className="alert alert-danger">{error.message}</div>
-			</ActivityMessageContainerComponent>
-		)
-	}
-	if (busy) {
-		content = (
-			<ActivityMessageContainerComponent>
-				<LoadingIndicator message="Loading..." />
-			</ActivityMessageContainerComponent>
-		)
-	}
+		if (innerContent !== null) {
+			return <ActivityMessageContainerComponent>{innerContent}</ActivityMessageContainerComponent>
+		}
 
-	if (response) {
-		if (
-			response.user &&
-			(window.location.pathname === '/login' || window.location.pathname === '/' || window.location.pathname === '')
-		) {
-			redirectUri = '/personal-info'
-		}
-		if (response.needsConfiguration && window.location.pathname !== '/oobe') {
-			redirectUri = '/oobe'
-		}
-		if (!response.needsConfiguration && window.location.pathname.toLowerCase() === '/oobe') {
-			redirectUri = '/'
-		}
-		if (response.needsAdmin && !response.needsConfiguration && window.location.pathname !== '/register') {
-			redirectUri = '/register'
-		}
-		// if (!redirectUri) {
-		// 	if (!response.needsConfiguration && !response.needsAdmin && !response.user && window.location.pathname !== '/login' && window.location.pathname !== '/register') {
-		// 		redirectUri = '/login'
-		// 	}
-		// }
-	}
-
-	if (redirectUri) {
-		content = (
-			<ActivityMessageContainerComponent>
-				<LoadingIndicator message="Redirecting..." />
-			</ActivityMessageContainerComponent>
-		)
-		Router.push(redirectUri)
-	}
+		return props.children
+	}, [busy, error, props.children, redirecting])
 
 	return (
 		<div>
